09e0df467605f999aea13d0e5ef5f29871be7e44
ioeftedal <ivar.eftedal@icloud.com>
2026-01-03 14:51:55 +0100

    chore: disabling lsp

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,10 @@
 local disabled = {
   "jay-babu/mason-null-ls.nvim",
   "nvimtools/none-ls.nvim",
-
   "nvim-neotest/neotest",
   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  "Saghen/blink.cmp",
+  "mfussenegger/nvim-dap-python",
 }
 
 return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, disabled)
